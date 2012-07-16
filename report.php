4331acd193cdeeb6594c5f01bbd195e683ba8581
Chris Cook <ccook5@cgs.rbksch.org>
2012-07-16 17:30:53 +0100

    Renamed last report as students. added initial work on new courses report. see issue 5 on github

--- report.php
+++ report.php
@@ -11,10 +11,48 @@ if (isset($_GET["type"])) {
 
 print_header($title = 'Coombe Sixth Form Enrolment', $hide_title_bar = false, $script = "");
 
+function print_students_for_course($course_id)
+{
+	global $config, $link;
+	$arr = array();
+	
+	$sql = "SELECT * FROM students INNER JOIN BLOCKS_CourseEnrolment ON students.id=BLOCKS_CourseEnrolment.StudentID WHERE BLOCKS_CourseEnrolment.CourseID='".$course_id."'";
+	
+//	print $sql;
+	$result = mysql_query($sql, $link);
+	
+	if (!$result)
+	{
+		die('Invalid query: ' . mysql_error()." On line ".__line__);
+	}
+	while ($row = mysql_fetch_array($result)) {		
+		$arr[] = $row['FirstName'];
+		print($row['FirstName']." ".$row['LastName']);
+	}
+}
+
 switch ($type)
 {
 	case "courses"  :
 		echo ("courses");
+		$sql = "SELECT * FROM BLOCKS_Blocks WHERE Year='".$config['current_year']."' ORDER BY id";
+		$result = mysql_query($sql, $link);
+		
+		while ($row = mysql_fetch_array($result)) {		
+			print("  <h1>".$row['Name'].".</h1>");
+			$sql2 = "SELECT * FROM BLOCKS_Course INNER JOIN BLOCKS_CourseDef ON CourseDefID=BLOCKS_CourseDef.id WHERE BlockID='".$row['id']."' AND EnrolmentYear='".$config['current_year']."'";
+			//print($sql2);
+			$result2 = mysql_query($sql2, $link);
+			
+			while ($row2 = mysql_fetch_array($result2)) {		
+				print("  <h1>".$row2['SubjectName']."</h1>");
+				print_students_for_course($row2['id']);
+			}
+		}
+		break;
+	case "students" :
+		echo ("students");
+		break;
 // sql query - get a list of student types
 		$sql = "SELECT * FROM StudentTypes";
 		$result = mysql_query($sql, $link);
@@ -63,9 +101,6 @@ switch ($type)
 			}
 			print("</table>");
 		}
-		break;
-	case "students" :
-		echo ("students");
 		break;
 	case "waiting"  :
 		echo ("waiting");
