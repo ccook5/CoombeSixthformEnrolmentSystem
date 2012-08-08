a6f7644ffa8890c4a0f34774ce6160c23c64e1a1
Chris Cook <ccook5@cgs.rbksch.org>
2012-08-08 14:46:56 +0100

    * updated code in courses report to make it more standard withing the project. * fixed a bug in students report, only one student from each type was being returned. This was caused by a vairable name being reused.

--- reports/students.php
+++ reports/students.php
@@ -6,6 +6,24 @@ require_once('../footer.inc.php');
 
 print_header($title = 'Coombe Sixth Form Enrolment', $hide_title_bar = false, $script = "");
 
+function get_students_courses($StudentID)
+{
+	global $link, $config;
+	
+	$sql3  = "SELECT * FROM BLOCKS_CourseEnrolment";
+	$sql3 .= " INNER JOIN BLOCKS_Course ON BLOCKS_Course.id=BLOCKS_CourseEnrolment.CourseID";
+	$sql3 .= " INNER JOIN BLOCKS_CourseDef ON BLOCKS_Course.CourseDefID=BLOCKS_CourseDEF.id";
+	$sql3 .= " WHERE StudentID='".$StudentID."';";
+	$res_gsc = mysql_query($sql3, $link) or die("Error: Problem with Query : ".$sql3);
+
+	$courses = array();
+	while ($row_gsc = mysql_fetch_array($res_gsc)) {
+		$courses[ $row_gsc['BlockID'] ] = $row_gsc['SubjectName'];
+	}
+	
+	return $courses;
+}
+
 // sql query - get a list of student types
 $sql = "SELECT StudentTypes.id, CourseType FROM StudentTypes INNER JOIN students ON StudentTypes.id=students.StudentType WHERE EnrolmentYear=2012 GROUP BY CourseType;";
 $result = mysql_query($sql, $link);
@@ -30,18 +48,14 @@ while ($row = mysql_fetch_array($result)) {
 //print table body
 	$sql3 = "SELECT * FROM students WHERE StudentType='".$row['id']."' AND EnrolmentYear='".$config['current_year']."'";
 	$res3 = mysql_query($sql3, $link) or die("error2");
-	while ($row3 = mysql_fetch_array($res3)) {
+	while ($row3 = mysql_fetch_array($res3))
+	{
 		print("   <tr>");	
 		print("    <td>".$row3["FirstName"]."</td>");	
 		print("    <td>".$row3["LastName"]."</td>");	
 		print("    <td></td>");
 		
-		$sql3 = "SELECT * FROM BLOCKS_CourseEnrolment INNER JOIN BLOCKS_Course ON BLOCKS_Course.id=BLOCKS_CourseEnrolment.CourseID INNER JOIN BLOCKS_CourseDef ON BLOCKS_Course.CourseDefID=BLOCKS_CourseDEF.id WHERE StudentID='".$row3['id']."' ";
-		$res3 = mysql_query($sql3, $link) or die("error3");
-		$courses = array();
-		while ($row3 = mysql_fetch_array($res3)) {
-			$courses[ $row3['BlockID'] ] = $row3['SubjectName'];
-		}
+		$courses = get_students_courses($row3['id']);
 		
 		foreach ($block_list as $block_id => $block) {
 			print("<td>");
@@ -50,7 +64,7 @@ while ($row = mysql_fetch_array($result)) {
 			}
 			print("</td>");	
 		}
-	print("</tr>");
+	    print("</tr>");
 	}
 	print("</table>");
 }
