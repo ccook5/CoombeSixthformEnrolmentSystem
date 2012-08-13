ffbd032febc5675a339751c274eea88fe5f98ce6
Chris Cook <ccook5@cgs.rbksch.org>
2012-08-13 11:03:56 +0100

    * prevent the waiting list report from showing if there is no one in the waiting list. * Merged all the html for the course table together so its more like a template.

--- reports/waiting.php
+++ reports/waiting.php
@@ -26,36 +26,40 @@ function print_students_for_course($course_id)
 {
 	global $config, $link;
 
-	print("    <table class='report-courses-students'>\n");
-
 	$sql    = "SELECT * FROM students INNER JOIN BLOCKS_CourseEnrolment ON students.id=BLOCKS_CourseEnrolment.StudentID";
     $sql   .= " WHERE BLOCKS_CourseEnrolment.CourseID='".$course_id."' AND students.EnrolmentYear='".$config['current_year']."' ORDER BY SequenceNumber;";
 	$result = mysql_query($sql, $link);
 
+	$rows = "";
+	
 	if ($result)
 	{
 		if (mysql_num_rows($result) > 0)
 		{
+			$i = 0;
+			$num_places = get_num_places($course_id);
+			while ($row3 = mysql_fetch_array($result))
+			{
+				if ($i >= $num_places && ($i - $num_places) < $config['waiting_list_length']) {
+					$rows .= "      <tr><td>".$row3['FirstName']."</td><td>".$row3['LastName']."</td><td>".$row3['SequenceNumber']."</td></tr>\n";
+				}
+				$i++;
+			}
+		}
+		if ($rows !== "") {
 ?>
+    <table class='report-courses-students'>
      <thead>
 	  <tr>
        <th>First Name</th><th>Last Name</th><th>Sequence Number</th>
 	  </tr>
      </thead>
      <tbody>
+	 <?php echo $rows; ?>
+     </tbody>
+	</table>
 <?php
-			$i = 0;
-			$num_places = get_num_places($course_id);
-			while ($row3 = mysql_fetch_array($result))
-			{
-				if ($i >= $num_places && ($i - $num_places) < $config['waiting_list_length']) {
-					print("      <tr><td>".$row3['FirstName']."</td><td>".$row3['LastName']."</td><td>".$row3['SequenceNumber']."</td></tr>\n");
-				}
-				$i++;
-			}
-			print("    </tbody>\n");
 		} else {print("-");}
-		print("   </table>\n");
 	}
 	else
 	{
@@ -69,7 +73,9 @@ $result = mysql_query($sql, $link);
 while ($row = mysql_fetch_array($result)) {
 	print("   <h1> Block: ".$row['Name'].".</h1>\n");
 	
-	$sql2 = "SELECT * FROM BLOCKS_Course INNER JOIN BLOCKS_CourseDef ON CourseDefID=BLOCKS_CourseDef.id WHERE BlockID='".$row['id']."' AND EnrolmentYear='".$config['current_year']."'";
+	$sql2  = "SELECT * FROM BLOCKS_Course";
+	$sql2 .= " INNER JOIN BLOCKS_CourseDef ON CourseDefID=BLOCKS_CourseDef.id";
+	$sql2 .= " WHERE BlockID='".$row['id']."' AND EnrolmentYear='".$config['current_year']."'";
 
 	$result2 = mysql_query($sql2, $link);
 	
